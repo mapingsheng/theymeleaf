b94c4dbcc5b69d2c201c5ac665a8cdfe24f38013
mapingsheng <mapingsheng1025@163.com>
2016-08-23 17:53:45 +0800

    添加了theymeleaf模板引擎的相关文章

--- src/main/java/controller/DoLoginController.java
+++ src/main/java/controller/DoLoginController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import entity.User;
+
 public class DoLoginController implements Controller{
 	 
     public DoLoginController() {
@@ -26,6 +28,12 @@ public class DoLoginController implements Controller{
         WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
         ctx.setVariable("username", username);
         ctx.setVariable("password", password);
+        
+        String[] types = {"足球","篮球","羽毛球"};
+        ctx.setVariable("types", types);
+        
+        User user = new User("zhang","san","china",25);
+        ctx.setVariable("user", user);
         
         templateEngine.process("desc", ctx, response.getWriter());//模板引擎转向到对应的模板页面
     }
